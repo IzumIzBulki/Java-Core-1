c086406edac2ac7e5e2e0d26e712afca1da5c164
IzumIzBulki <118291913+IzumIzBulki@users.noreply.github.com>
2023-11-06 15:37:00 +0300

    Delete JavaCore#2/src/main/java directory

--- JavaCore#2/src/main/java/org /example/customExceptions/WrongBuyerException.java
+++
@@ -1,7 +0,0 @@
-package org.example.customExceptions;
-
-import java.io.IOException;
-
-public class WrongBuyerException extends IOException {
-    public WrongBuyerException(String message){super(message);}
-}
